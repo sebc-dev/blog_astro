0d6997c4aa92c93d38102091cabd2b4055e6019b
NegusSalomon <chauveau.sebastien@gmail.com>
2025-06-03 08:14:09 +0200

    🔧 ConfigESLint

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+import xoConfig from 'eslint-config-xo';
+import astroPlugin from 'eslint-plugin-astro';
+import tsParser from '@typescript-eslint/parser';
+import { defineConfig } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default defineConfig([
+  ...xoConfig,
+  prettierConfig,
+
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+      },
+    },
+
+    plugins: {
+      astro: astroPlugin,
+    },
+
+    files: ['**/*.{js,ts}'],
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroPlugin.parser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    rules: {
+      ...(astroPlugin.configs?.recommended?.rules || {}),
+      '@stylistic/curly-newline': 'off',
+    },
+  },
+  {
+    files: ['**/*.test.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@stylistic/curly-newline': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@stylistic/curly-newline': 'off',
+    },
+  },
+]);
